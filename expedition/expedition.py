770bfdd3768d7813663add1540f65e24d98dc85d
marshal <kamocat@gmail.com>
2024-12-08 21:39:14 -0800

    Routes to the wrong path??

--- expedition/expedition.py
+++ expedition/expedition.py
@@ -1,24 +1,59 @@
 import rclpy
 from rclpy.node import Node
+import numpy as np
 
-from nav_msgs.msg import OccupancyGrid
-import plan
+from nav_msgs.msg import OccupancyGrid, Odometry, Path
+from geometry_msgs.msg import *
+from . import plan
 
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('expedition')
-        self.subscription = self.create_subscription(
+        self.pos = Point()
+        self.sub = [
+                self.create_subscription(
             OccupancyGrid,
             'map',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
+            self.create_path,
+            1),
+                self.create_subscription(
+            Odometry,
+            'odom',
+            self.get_pos,
+            1),
+        ]
 
-    def listener_callback(self, msg):
-        path = plan.plan(msg.data)
-        self.get_logger().info(f'{msg.info}')
+        self.pub = [
+                self.create_publisher(
+                    Path, 'plan', 10)
+                ]
+        # prevent unused variable warning
+        self.pub
+        self.sub 
+
+    def create_path(self, msg):
+        #FIXME: Account for map orientation
+        x = self.pos.x - msg.info.origin.position.x
+        y = self.pos.y - msg.info.origin.position.y
+        k = 1 / msg.info.resolution
+        map1 = np.asarray(msg.data).reshape((msg.info.width, msg.info.height))
+        points = plan.plan(map1, int(x*k), int(y*k), 1)
+        def toPose(a):
+            b=PoseStamped()
+            b.pose.position.x = a[0]*msg.info.resolution + msg.info.origin.position.x
+            b.pose.position.y = a[1]*msg.info.resolution + msg.info.origin.position.y
+            return b
+        path = Path()
+        path.header = msg.header
+        path.poses = [toPose(x) for x in points]
+        self.pub[0].publish(path)
+
+
+    def get_pos(self, msg):
+        self.pos = msg.pose.pose.position
+        #self.get_logger().info(f'position: ({self.pos.x:0.3f},{self.pos.y:0.3f},{self.pos.z:0.3f})')
 
 
 def main(args=None):
--- expedition/plan.py
+++ expedition/plan.py
@@ -1,39 +1,47 @@
 import numpy as np
+#from PIL import Image, ImageShow
 
-def plan(mymyap, pos):
-    wall = 0
-    clear = 254
-    unexplored = 205
+def plan(mymap, x, y, width):
+    #print((x,y,mymap.shape, width))
+    #im = Image.fromarray(mymap)
+    #im.show()
+    colors = list(np.unique(mymap))
+    clear = mymap[x,y] #Assume the robot is not on a wall
+    unexplored = mymap[0,0] #Assume the corner is unexplored
+    colors.remove(clear)
+    colors.remove(unexplored)
+    wall = colors[0] #Remaining color must be clear
 
+    #print(f'clear:{clear}, wall:{wall}, unexplored:{unexplored}')
 
-    mymap
     plan = np.zeros(mymap.shape, dtype=np.uint8) #Could choose uint16 if we need a longer path
 
-    x = plan.shape[1]//2
-    y = plan.shape[0]//2
     s = 255
     plan[x][y] = s
 
     def find_unexplored():
-        for i in range(1,s): #Repeat until we find our unexplored area
-            for x1 in range(max(x-i,1),min(x+i+1,x+x)): #Traverse over every column 
-                for y1 in range(max(y-i,1),min(x+i+1,x+x)): #Traverse over every row
+        for i in range(1,50): #Repeat until we find our unexplored area
+            j = 0
+            for x1 in range(max(x-i,1),min(x+i+1,mymap.shape[0])): #Traverse over every column 
+                for y1 in range(max(y-i,1),min(y+i+1,mymap.shape[1])): #Traverse over every row
                     #If not touching the wall but next to explored path
-                    x2=x1-1
-                    x3=x1+2
-                    y2=y1-1
-                    y3=y1+2
+                    x2=x1-width
+                    x3=x1+width+1
+                    y2=y1-width
+                    y3=y1+width+1
                     if wall not in mymap[x2:x3,y2:y3] and s+1-i in plan[x2:x3,y2:y3]:
                         if plan[x1][y1] == 0:
                             plan[x1][y1] = s-i #Mark as next step in plan
+                            j += 1
                         if mymap[x1][y1] == unexplored:
                             return(x1,y1) #Return the coordinates of the closest unexplored area
+            assert j > 0, f'No path found at depth {i}'
 
 
     def retrace_steps(x,y):
         p = [(x,y)]
         for i in range(plan[x1,y1],s):
-            n = np.argmax(plan[x-1:x+2,y-1:y+2])
+            n = np.argmax(plan[x-width:x+width+1,y-width:y+width+1])
             x = n//3 + x-1
             y = n%3 + y-1
             p.append((x,y))
@@ -42,8 +50,5 @@ def plan(mymyap, pos):
 
     x1, y1 = find_unexplored()
     steps = retrace_steps(x1,y1)
-
-    plan2= np.zeros(mymap.shape, dtype=np.uint8)
-    for i in steps:
-        plan2[i] = 128
-    return plan2
+    steps.reverse()
+    return steps
